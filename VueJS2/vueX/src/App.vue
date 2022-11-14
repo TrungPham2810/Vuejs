d230c926650a43dc18850b1a1b0f051e45cf904b
trungpham <trungphamvan2810@gmail.com>
2022-11-14 20:16:46 +0700

    vueX

---
+++ VueJS2/vueX/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 ">
+        <transition name="slide">
+          <router-view/>
+        </transition>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+
+    export default {
+        name: 'app',
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style>
+  /*Slide transition*/
+  .slide-enter {
+
+  }
+  .slide-enter-active {
+    animation: slide-in .2s ease-out forwards;
+    position: absolute;
+  }
+
+  .slide-leave-to {
+
+  }
+  .slide-leave-active {
+    animation: slide-out .2s ease-out forwards;
+  }
+
+  @keyframes slide-in {
+    from{
+      transform: translateX(20px);
+      opacity: 0;
+    }
+    to{
+      transform: translateX(0);
+    }
+  }
+
+  @keyframes slide-out {
+    from{
+      transform: translateX(0);
+
+    }
+    to{
+      transform: translateX(20px);
+      opacity: 0;
+    }
+  }
+</style>
+
